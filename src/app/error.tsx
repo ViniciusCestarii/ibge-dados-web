5ab038f2121829918c4bb546495155ad07e3d1be
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-09-02 15:05:11 -0300

    feat: add sentry and improve error handler

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,6 @@
-'use client' // Error boundaries must be Client Components
+'use client'
 
+import { Button } from '@/components/ui/button'
 import { useEffect } from 'react'
 
 export default function Error({
@@ -10,14 +11,28 @@ export default function Error({
   reset: () => void
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error)
   }, [error])
 
+  const handleClearQueryParameters = () => {
+    window.location.href = '/'
+    reset()
+  }
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button onClick={() => reset()}>Try again</button>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+      <h2 className="text-3xl font-bold text-destructive mb-4">
+        Oops! Parece que deu algo de errado.
+      </h2>
+      <p className="text-lg mb-2">
+        Desculpe pela inconveniência. Nossa equipe já foi notificada e estamos
+        trabalhando para resolver o problema.
+      </p>
+      {error.digest && <p className="text-sm">Error Code: {error.digest}</p>}
+      <div className="mt-6 flex gap-4 flex-wrap justify-center">
+        <Button onClick={() => reset()}>Tentar de novo</Button>
+        <Button onClick={handleClearQueryParameters}>Limpar campos</Button>
+      </div>
+    </main>
   )
 }
