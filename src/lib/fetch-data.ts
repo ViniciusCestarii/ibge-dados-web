1579f45db48d734082f3eb1272506e42d887fbfa
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-09-04 14:12:51 -0300

    fix: fetch geo json

--- src/lib/fetch-data.ts
+++ src/lib/fetch-data.ts
@@ -4,14 +4,11 @@ import { ONE_DAY_IN_SECONDS } from './utils'
 export async function fetchGeoJsonMap(
   nivelId: NivelId,
 ): Promise<LocalGeografico[]> {
-  const response = await fetch(
-    `http://localhost:3000/api/geo-json/${nivelId}`,
-    {
-      next: {
-        revalidate: ONE_DAY_IN_SECONDS,
-      },
+  const response = await fetch(`/api/geo-json/${nivelId}`, {
+    next: {
+      revalidate: ONE_DAY_IN_SECONDS,
     },
-  )
+  })
   const data = await response.json()
   return data
 }
