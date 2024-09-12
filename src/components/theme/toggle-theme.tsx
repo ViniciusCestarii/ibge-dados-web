95e95c928350671ffc6589ef6413cb7fbc1cbbe0
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-09-12 17:31:21 -0300

    feat: fix eslint

--- src/components/theme/toggle-theme.tsx
+++ src/components/theme/toggle-theme.tsx
@@ -18,7 +18,7 @@ export function ToggleTheme() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" aria-label='mudar tema'>
+        <Button variant="outline" size="icon" aria-label="mudar tema">
           <Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
