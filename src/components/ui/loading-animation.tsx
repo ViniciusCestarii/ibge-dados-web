f720cfa04b4d002bf523a813fbdf5e40efd7f648
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-09-12 13:09:05 -0300

    feat: improve loading animation

--- src/components/ui/loading-animation.tsx
+++ src/components/ui/loading-animation.tsx
@@ -1,6 +1,8 @@
 'use client'
+import { cn } from '@/lib/utils'
 import { Player } from '@lottiefiles/react-lottie-player'
 import { useTheme } from 'next-themes'
+import { useState } from 'react'
 
 const lightAnimation =
   'https://lottie.host/4b5d1abc-8830-4731-a428-492a30d31340/xvqhSDXKPT.json'
@@ -8,18 +10,28 @@ const darkAnimation =
   'https://lottie.host/322faed9-3c2f-4b6d-a559-01db4673a426/2nYNSPPfxp.json'
 
 const LoadingAnimation = () => {
-  const theme = useTheme()
+  const { resolvedTheme } = useTheme()
+  const [fadeIn, setFadeIn] = useState(false)
 
-  const isDark = theme.resolvedTheme === 'dark'
-
+  const isDark = resolvedTheme === 'dark'
   const animation = isDark ? darkAnimation : lightAnimation
+
+  const handlePlayerEvent = (event: string) => {
+    if (event === 'frame') {
+      setFadeIn(true)
+    }
+  }
 
   return (
     <Player
+      onEvent={handlePlayerEvent}
       autoplay
       loop
       src={animation}
-      className="max-w-screen-sm w-full aspect-square p-12 animate-fade-in"
+      className={cn(
+        'opacity-0 max-w-screen-sm w-full aspect-square p-12',
+        fadeIn && 'opacity-100 transition-opacity duration-500',
+      )}
     />
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,15 +75,10 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
-        'fade-in': {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in': 'fade-in 1.8s cubic-bezier(0.64, 0, 0.78, 0)',
       },
     },
   },
